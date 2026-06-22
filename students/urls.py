295dd043d6723c2564c562ad02137f5ad686697e
Manoj P <manojpainam1221@gmail.com>
2026-06-22 22:48:53 +0530

    Added get all students, student by id, base html for disaplying the content

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('greet_student/', views.greet_student, name='students'),
-    path('get_all_students/', views.get_all_students, name='get_all_students')
+    path('get_all_students/', views.get_all_students, name='get_all_students'),
+    path('student/<int:id>', views.get_details, name="get_details")
 ]
--- students/views.py
+++ students/views.py
@@ -10,3 +10,8 @@ def get_all_students(request):
     students = Student.objects.all().values()
     data = {"students" : students}
     return render(request, 'all_students.html', context = data)
+
+def get_details(request, id):
+    student = Student.objects.get(id=id)
+    data = {"student" : student}
+    return render(request, 'details.html', context = data)
