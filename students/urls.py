7b0dc03d90de681e562fd80442fcf22439b69e1e
Manoj P <manojpainam1221@gmail.com>
2026-06-27 23:55:50 +0530

    Query set with filters and static files has been added

--- students/urls.py
+++ students/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('testing/', views.testing, name="testing"),
     path('greet_participant/', views.greet_participant, name='greet_participant'),
     path('all/', views.print_all_participants, name='print_all_participants'),
-    path('get_fristnames', views.get_firstnames, name='get_fristnames')
+    path('get_fristnames', views.get_firstnames, name='get_fristnames'),
+    path('get_student/<id>', views.get_stduent_by_id, name="get_student_by_id"),
+    path('get_student/<id>/<fname>', views.get_stduent_by_id_and_firstname, name="get_stduent_by_id_and_firstname"),
+    path('get_student_by_fname_or_lname/<fname>/<lname>', views.get_student_by_fname_or_lname, name="get_student_by_fname_or_lname"),
+    path('get_fruits', views.load_static, name="load_static")
+
 ]
--- students/views.py
+++ students/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Student
+from django.db.models import Q
 
 
 def greet_student(request):
     return render(request, 'home.html')
 
 def get_all_students(request):
-    students = Student.objects.all().values()
+    #students = Student.objects.all().values()
+
+    #ASEC - can also order by different values like fname
+    #students = Student.objects.all().order_by('fname').values()
+
+    #DESC - descending order
+    students = Student.objects.all().order_by('-fname').values()
     data = {"students" : students}
     return render(request, 'all_students.html', context = data)
 
@@ -50,3 +57,49 @@ def get_firstnames(request):
     }
 
     return render(request, "get_firstnames.html", context)
+
+'''
+Get student by Id by using the Query set methods
+'''
+def get_stduent_by_id(request, id):
+    #filter.values will give the query set which we can't loop in query set in the template
+    student = Student.objects.filter(id=id).first()
+    data = {
+        "student" : student
+    }
+    if student:
+        return render(request, "details.html", context = data)
+    
+    return render(request, "404.html")
+
+
+'''
+Query set filter 
+'''
+#AND : this function will let's you to search the student by knowing their name and id
+def get_stduent_by_id_and_firstname(request, id, fname):
+    # we are using fname__iexact exact in URL we can't pass the caps
+    student = Student.objects.filter(fname__iexact=fname, id=id).first()
+    if student:
+        return render(request, "details.html", context = {"student" : student})
+    
+    return render(request, "404.html")
+
+#OR
+def get_student_by_fname_or_lname(request, fname, lname):
+    student = Student.objects.filter(Q(fname__iexact=fname) | Q(lname__iexact=lname)).first()
+
+    #can also use fname__startwith
+
+    if student:
+        return render(request, "details.html", context = {"student" : student})
+    
+    return render(request, "404.html")
+
+def load_static(request):
+    fruits = ['apple', 'banana', 'cherry']
+    
+    if fruits:
+        return render(request, "fruits.html", context = {"fruits" : fruits})
+    
+    return render(request, "404.html")
