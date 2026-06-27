d4786ba54ab0440c739f84844e6d8c2ae273093f
Manoj P <manojpainam1221@gmail.com>
2026-06-28 00:38:27 +0530

    Added a username column

--- students/models.py
+++ students/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    username = models.CharField(max_length=20, null=False, unique=True)
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=100)
-    mobile = models.IntegerField(null=True)
+    mobile = models.BigIntegerField(null=True)
 
     def __str__(self):
         return "{} {}".format(self.fname, self.lname)
